4b975ffab9352b373d709dc57fe8401766391cff
Tucker Mogren <mogrent@hotmail.com>
2023-08-26 19:38:12 -0400

    generating projects for unit tests

---
+++ src/TicketMapper.Test.Domain/TicketMapper.Test.Application/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketMapper.Test.Application;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ src/TicketMapper.Test.Domain/TicketMapper.Test.WebApi/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace TicketMapper.Test.WebApi;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
