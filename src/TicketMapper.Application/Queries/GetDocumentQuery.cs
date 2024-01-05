126d4ae0277b5f1db0926f8577798f5afc58ccbc
Tucker Mogren <mogrent@hotmail.com>
2024-01-04 20:38:30 -0500

    tests for GetDocumentQueryTests.cs

--- src/TicketMapper.Application/Queries/GetDocumentQuery.cs
+++ src/TicketMapper.Application/Queries/GetDocumentQuery.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using System.IO.Abstractions;
+using MediatR;
+using Microsoft.Extensions.Logging;
 using TicketMapper.Domain.Interfaces.Queries;
 
 namespace TicketMapper.Application.Queries
@@ -7,16 +9,45 @@ namespace TicketMapper.Application.Queries
     {
         public string Path { get; set; } = path ?? throw new ArgumentNullException(nameof(path));
 
-        public class GetDocumentQueryHandler : IRequestHandler<IGetDocumentsQuery<byte[]>, byte[]>
+        public class GetDocumentQueryHandler(IFileSystem fileSystem, ILogger<GetDocumentQueryHandler> logger)
+            : IRequestHandler<IGetDocumentsQuery<byte[]>, byte[]>
         {
             public async Task<byte[]> Handle(IGetDocumentsQuery<byte[]> request, CancellationToken cancellationToken)
             {
-                return await ReadFileToByteArray(request.Path);
+                var file = Array.Empty<byte>();
+                
+                try
+                {
+                    if (string.IsNullOrWhiteSpace(request.Path))
+                    {
+                        throw new ArgumentNullException(nameof(request.Path));
+                    }
+
+                    logger.LogInformation($"Starting to get the document at path: {request.Path}");
+                    file = await ReadFileToByteArray(request.Path);
+                }
+                catch (ArgumentNullException e)
+                {
+                    logger.LogCritical(message: e.Message);
+                    throw;
+                }
+                catch (OperationCanceledException e)
+                {
+                    logger.LogCritical($"Cancellation token received: {e.Message}");
+                    throw;
+                }
+                catch (Exception e)
+                {
+                    logger.LogCritical($"Issue reading file: {e.Message}");
+                    throw;
+                }
+
+                return file;
             }
 
             private async Task<byte[]> ReadFileToByteArray(string filePath)
             {
-                return await File.ReadAllBytesAsync(filePath);
+                return await fileSystem.File.ReadAllBytesAsync(filePath);
             }
         }
 
