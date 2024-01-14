d3ae3050709c48dd7ace4c16e3da3b050dccea6b
Tucker Mogren <mogrent@hotmail.com>
2024-01-14 17:19:58 -0500

    Starting the config of GoogleDrive DriveService

--- src/TicketMapper.WebApi/Configs/ConfigureGoogleDrive.cs
+++ src/TicketMapper.WebApi/Configs/ConfigureGoogleDrive.cs
@@ -1,6 +1,20 @@
+using Google.Apis.Drive.v3;
 namespace TicketMapper.WebApi.Configs;
 
 public static class ConfigureGoogleDrive
 {
-    
+    public static DriveService GoogleDriveConfiguration(this ServiceCollection services)
+    {
+        services.AddSingleton<DriveService>(provider => 
+        {
+            // Create and configure your DriveService instance here
+            UserCredential credential = // ... obtain credentials ...
+            return new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "GoogleDriveAPI",
+            });
+            
+        });
+    }
 }
